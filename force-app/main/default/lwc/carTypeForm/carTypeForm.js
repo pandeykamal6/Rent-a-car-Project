79c66ecf8bbc8908de529e0153369cbe4f1504e7
Kamal Pandey <pandeykamal6@gmail.com>
2023-05-27 17:45:45 +0530

    adding rest of items to other path

--- force-app/main/default/lwc/carTypeForm/carTypeForm.js
+++ force-app/main/default/lwc/carTypeForm/carTypeForm.js
@@ -1,3 +1,10 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import queryAllCars from '@salesforce/apex/GetAllCars.getCars';
+const carFields = [{ label: 'Car Name', fieldName: 'Name', type: 'text' }, { label: 'Owner Name', fieldName: 'Contact__r.Name', type: 'text' }];
+export default class CarTypeForm extends LightningElement {
+    carFields = carFields;
+    error;
+    @wire(queryAllCars)
+    carDataValues;
 
-export default class CarTypeForm extends LightningElement {}
+}
